b3317e5e4756afe62073c141aea6f1c30b100d1e
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-02-23 11:26:09 +0700

    feat: add image and README.md movie

--- query1.sql
+++ query1.sql
@@ -3,10 +3,10 @@
 --Mencari movie dengan rating diantara 5 dan 7 dan tahun rilis 2004 sampai 2006
 --Menghitung jumlah movie dengan rating 6
 
-SELECT "name", "year" FROM movies WHERE "year" >= '2000'
+SELECT "name", "year" FROM movies WHERE "year" >= '2000' ORDER BY "year"
 
-SELECT "first_name", "last_name" FROM "actors" WHERE "first_name"  LIKE '%s'
+--SELECT "first_name", "last_name" FROM "actors" WHERE "first_name"  LIKE '%s'
 
-SELECT "rankscore", "year" FROM "movies" WHERE "rankscore" BETWEEN 5 AND  7 AND "year" BETWEEN 2004 AND 2006
+--SELECT "rankscore", "year" FROM "movies" WHERE "rankscore" BETWEEN 5 AND  7 AND "year" BETWEEN 2004 AND 2006
 
-SELECT COUNT("rankscore") FROM "movies" WHERE "rankscore" = 6
+--SELECT COUNT("rankscore") FROM "movies" WHERE "rankscore" = 6
